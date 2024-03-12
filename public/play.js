d75be5c5c0dd6ac1aed21fb31a1ce89e95d396b6
colintsmith <colin21199@gmail.com>
2024-03-11 20:58:32 -0700

    updated play.js

--- public/play.js
+++ public/play.js
@@ -103,7 +103,7 @@ class Game {
     }
 
     computerWin() {
-        this.saveScores();
+        this.saveScore();
         this.roundsWon = 0;
         console.log("computer selected " + this.computerItem + " computer won");
         const answerBox = document.getElementById("answerBox");
@@ -144,6 +144,28 @@ class Game {
         //    console.log('nothing in local storage')
         //}
         console.log('end save scores');
+    }
+    async saveScore(){
+        score = this.roundsWon
+        const userName = this.getPlayerName();
+        const date = new Date().toLocaleDateString();
+        const newScore = {name: userName, score: score, date: date};
+
+        try {
+            const response = await fetch('/api/score', {
+              method: 'POST',
+              headers: {'content-type': 'application/json'},
+              body: JSON.stringify(newScore),
+            });
+      
+            // Store what the service gave us as the high scores
+            const scores = await response.json();
+            localStorage.setItem('scores', JSON.stringify(scores));
+          } catch {
+            // If there was an error then just track scores locally
+            //this.updateScoresLocal(newScore);
+            console.log('something went wrong in saveScore')
+          }
     }
     /*
     updateScores(userName, score, scores) {
